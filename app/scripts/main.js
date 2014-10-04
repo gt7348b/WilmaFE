e413ecffaab3dd5e6acc6f44aaa1a8ceab62734c
John Crocker <johnniecrocker@yahoo.com>
2014-10-04 15:52:41 -0400

    Got menues to show up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('The Iron Yard Rocks');
+
+$( "#Button1" ).click(function() {
+        $('#Button1').toggleClass("click");
+        $("#Button2").removeClass("click");
+        $("#Button3").removeClass("click");
+});
+
+$( "#Button2" ).click(function() {
+        $("#Button2").toggleClass("click");
+        $("#Button1").removeClass("click");
+        $("#Button3").removeClass("click");
+});
+
+$( "#Button3" ).click(function() {
+        $("#Button3").toggleClass("click");
+        $("#Button1").removeClass("click");
+        $("#Button2").removeClass("click");
+});
